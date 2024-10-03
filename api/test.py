d71d77efa2a083d7e5297a73aa944acbdea6e57a
svparekh <setulvp@gmail.com>
2024-10-03 11:28:30 -0500

    pipeline and test.py changes

--- api/test.py
+++ api/test.py
@@ -19,9 +19,11 @@ This test suite covers the following endpoints:
 
 # pylint: disable = line-too-long, too-many-lines, no-name-in-module, multiple-imports, pointless-string-statement, wrong-import-order, trailing-whitespace, invalid-name, too-many-public-methods, no-else-return, no-else-break
 
+import sys
 import unittest
 import json
-from api import app
+import psycopg2
+from api import app, POSTGRES_URL
 
 
 created_projects = set()
@@ -34,16 +36,32 @@ class TestAPI(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
+        create_test_data()
 
     def tearDown(self):
-        for project in self.app.get("/projects").json["message"]:
-            created_projects.add(project[0])
-        # Delete all projects
-        for project_id in created_projects:
-            response = self.app.delete(f"/projects/{project_id}")
-            self.assertEqual(response.status_code, 204)
+        """
+        Clean up the database after each test
+        """
+        # DELETE ALL DATA CREATED BY THE TESTS
+        # This actually deletes all data in the database, since the reset test already is deleting all data
+        try:
+            # Attempt to connect to the database
+            conn = psycopg2.connect(POSTGRES_URL)
+            # Create a cursor
+            cur = conn.cursor()
+            # Delete all data in the database
+            cur.execute("DELETE FROM issues")
+            cur.execute("DELETE FROM projects")
+            # Commit the changes
+            conn.commit()
+            cur.close()
+            conn.close()
+        # If database has connection or other error
+        except psycopg2.Error as e:
+            print(e)
+            sys.exit(-1)
 
-    def test_reset(self):
+    def test_reset_endpoint(self):
         """
         Test that the reset endpoint deletes all data in the database
         and returns a 204 status code.
@@ -54,6 +72,8 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 204)
         self.assertEqual(self.app.get("/projects").json["message"], [])
         self.assertEqual(self.app.get("/issues").json["message"], [])
+        # If reset passes, recreate the data needed for other tests
+        create_test_data()
 
     def test_root_endpoint(self):
         """
@@ -87,11 +107,6 @@ class TestAPI(unittest.TestCase):
         Test that the get projects endpoint returns a 200 status code and a JSON response
         with a list of all projects in the database when given valid data.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
         # Call
         response = self.app.get("/projects")
         # Test
@@ -103,76 +118,46 @@ class TestAPI(unittest.TestCase):
         Test that the get project by ID endpoint returns a 200 status code and a JSON response
         with the project's data when given a valid ID.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
         # Call
-        response = self.app.get(f"/projects/{project_id}")
+        response = self.app.get("/projects/1")
         # Test
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json["message"], list)
-        self.assertEqual(response.json["message"][0], id)
+        self.assertEqual(response.json["message"][0], 1)
 
     def test_update_project(self):
         """
         Test that the update project endpoint returns a 200 status code and a JSON response
         with a message indicating that the data was updated successfully when given valid data.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
         # Call
         project_data = {
-            "name": "Updated Test Project",
-            "description": "This is an updated test project",
+            "name": "Updated Test Project 1",
+            "description": "This is an updated test Project 1",
         }
         response = self.app.put(
-            f"/projects/{project_id}",
+            "/projects/1",
             data=json.dumps(project_data),
             content_type="application/json",
         )
         # Get the updated project
-        response_updated = self.app.get(f"/projects/{project_id}")
+        response_updated = self.app.get("/projects/1")
         # Test
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json["message"], "Data updated successfully")
-        self.assertEqual(response_updated.json["message"][1], "Updated Test Project")
+        self.assertEqual(response_updated.json["message"][1], "Updated Test Project 1")
 
     def test_delete_project(self):
         """
         Test that the delete project endpoint returns a 204 status code and deletes the
         project and all its associated issues from the database when given a valid ID.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
-        # Create an issue in the project
-        issue_data = {"title": "Test Issue", "description": "This is an test issue"}
-        response = self.app.post(
-            f"/projects/{project_id}/issues",
-            data=json.dumps(issue_data),
-            content_type="application/json",
-        )
-        # Get issue id
-        issue_id = self.app.get(f"/projects/{project_id}/issues").json["message"][0][0]
         # Call
-        response = self.app.delete(f"/projects/{project_id}")
+        response = self.app.delete("/projects/2")
         # Ensure deletion of project
-        response_del = self.app.get(f"/projects/{project_id}")
+        response_del = self.app.get("/projects/2")
         # # Ensure deletion of issue
-        response_issue_del = self.app.get(f"/issues/{issue_id}")
+        response_issue_del = self.app.get("/issues/2")
         # Test
         self.assertEqual(response.status_code, 204)
         self.assertEqual(response_del.status_code, 404)
@@ -183,17 +168,10 @@ class TestAPI(unittest.TestCase):
         Test that the create issue endpoint returns a 201 status code and a JSON response
         with a message indicating that the data was added successfully when given valid data.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        response = self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
         # Call
         issue_data = {"title": "Test Issue", "description": "This is a test issue"}
         response = self.app.post(
-            f"/projects/{project_id}/issues",
+            "/projects/1/issues",
             data=json.dumps(issue_data),
             content_type="application/json",
         )
@@ -206,46 +184,18 @@ class TestAPI(unittest.TestCase):
         Test that the get issues by project endpoint returns a 200 status code and a JSON response
         with a list of all issues in the project when given valid data.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        response = self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
-        # Create an issue in the project
-        issue_data = {"title": "Test Issue", "description": "This is a test issue"}
-        response = self.app.post(
-            f"/projects/{id}/issues",
-            data=json.dumps(issue_data),
-            content_type="application/json",
-        )
         # Call
-        response = self.app.get(f"/projects/{project_id}/issues")
+        response = self.app.get("/projects/1/issues")
         # Test
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json["message"], list)
-        self.assertEqual(response.json["message"][0][1], project_id)
+        self.assertEqual(response.json["message"][0][1], 1)
 
     def test_get_issues(self):
         """
         Test that the get issues endpoint returns a 200 status code and a JSON response
         with a list of all issues in the database when given valid data.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        response = self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
-        # Create an issue in the project
-        issue_data = {"title": "Test Issue", "description": "This is a test issue"}
-        response = self.app.post(
-            f"/projects/{project_id}/issues",
-            data=json.dumps(issue_data),
-            content_type="application/json",
-        )
         # Call
         response = self.app.get("/issues")
         # Test
@@ -257,92 +207,46 @@ class TestAPI(unittest.TestCase):
         Test that the get issue by ID endpoint returns a 200 status code and a JSON response
         with the issue's data when given a valid ID.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        response = self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
-        # Create an issue in the project
-        issue_data = {"title": "Test Issue", "description": "This is a test issue"}
-        response = self.app.post(
-            f"/projects/{project_id}/issues",
-            data=json.dumps(issue_data),
-            content_type="application/json",
-        )
-        # Get issue id
-        issue_id = self.app.get("/issues").json["message"][0][0]
         # Call
-        response = self.app.get(f"/issues/{issue_id}")
+        response = self.app.get("/issues/1")
         # Test
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json["message"], list)
-        self.assertEqual(response.json["message"][0], issue_id)
+        self.assertEqual(response.json["message"][0], 1)
 
     def test_update_issue(self):
         """
         Test that the update issue endpoint returns a 200 status code and a JSON response
         with a message indicating that the data was updated successfully when given valid data.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        response = self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
-        # Create an issue in the project
-        issue_data = {"title": "Test Issue", "description": "This is a test issue"}
-        response = self.app.post(
-            f"/projects/{project_id}/issues",
-            data=json.dumps(issue_data),
-            content_type="application/json",
-        )
-        # Get issue id
-        issue_id = self.app.get("/issues").json["message"][0][0]
         # Call
         issue_data = {
-            "title": "Updated Test Issue",
-            "description": "This is an updated test issue",
+            "title": "Updated Test Issue 1 for Project 1",
+            "description": "This is an updated test issue 1 for project 1",
         }
         response = self.app.put(
-            f"/issues/{issue_id}",
+            "/issues/1",
             data=json.dumps(issue_data),
             content_type="application/json",
         )
         # Get updated issue
-        response_updated = self.app.get(f"/issues/{issue_id}")
+        response_updated = self.app.get("/issues/1")
         # Test
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json["message"], "Data updated successfully")
-        self.assertEqual(response_updated.json["message"][2], "Updated Test Issue")
+        self.assertEqual(
+            response_updated.json["message"][2], "Updated Test Issue 1 for Project 1"
+        )
 
     def test_delete_issue(self):
         """
         Test that the delete issue endpoint returns a 204 status code and deletes the
         issue from the database when given a valid ID.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        response = self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
-        # Create an issue in the project
-        issue_data = {"title": "Test Issue", "description": "This is a test issue"}
-        response = self.app.post(
-            f"/projects/{project_id}/issues",
-            data=json.dumps(issue_data),
-            content_type="application/json",
-        )
-        # Get issue id
-        issue_id = self.app.get("/issues").json["message"][0][0]
         # Call
-        response = self.app.delete(f"/issues/{issue_id}")
+        response = self.app.delete("/issues/3")
         # # Ensure deletion of issue
-        response_issue_del = self.app.get(f"/issues/{issue_id}")
+        response_issue_del = self.app.get("/issues/3")
         # Test
         self.assertEqual(response.status_code, 204)
         self.assertEqual(response_issue_del.status_code, 404)
@@ -379,16 +283,9 @@ class TestAPI(unittest.TestCase):
         Test that the create issue endpoint returns a 400 status code and an error
         message when given empty data.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        response = self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
         # Call
         response = self.app.post(
-            f"/projects/{project_id}/issues",
+            "/projects/1/issues",
             data=json.dumps({}),
             content_type="application/json",
         )
@@ -411,22 +308,83 @@ class TestAPI(unittest.TestCase):
         Test that the create issue endpoint returns a 400 status code and an error
         message when given invalid data.
         """
-        # Create a project
-        project_data = {"name": "Test Project", "description": "This is a test project"}
-        response = self.app.post(
-            "/projects", data=json.dumps(project_data), content_type="application/json"
-        )
-        # Get a project id to use
-        project_id = self.app.get("/projects").json["message"][0][0]
         # Call
         issue_data = {"invalid-key": "Invalid value"}
         response = self.app.post(
-            f"/projects/{project_id}/issues",
+            "/projects/1/issues",
             data=json.dumps(issue_data),
             content_type="application/json",
         )
         # Test
         self.assertEqual(response.status_code, 400)
+
+
+def create_test_data():
+    """
+    Creates database items to be used by the tests.
+
+    This function creates three projects (ids 1, 2, and 3) and three issues (ids 1:1, 2:2, and 3:3)
+    for each project to be used by the tests.
+
+    If the database has a connection or other error, prints the error and exits the program with a status code of -1.
+
+    Project 1 is used for:
+      - Get all projects
+      - Create project
+      - Get project by ID
+      - Update project
+      - Get all issues
+      - Get issues by project
+      - Create issue
+      - Update issue
+      - Invalid issue data
+      - Empty issue data
+      - Invalid project data
+      - Empty project data
+    Project 2 is used for:
+      - Delete project
+    Project 3 is used for:
+      - Delete issue
+    """
+    try:
+        # Attempt to connect to the database
+        conn = psycopg2.connect(POSTGRES_URL)
+        # Create a cursor
+        cur = conn.cursor()
+
+        # Create three projects (ids 1 ,2, and 3)
+        cur.execute(
+            """
+            INSERT INTO projects (id, name, description)
+            VALUES
+                (1, 'Test Project 1', 'This is test project 1'),
+                (2, 'Test Project 2', 'This is test project 2'),
+                (3, 'Test Project 3', 'This is test project 3')
+            ON CONFLICT (id) DO NOTHING
+        """
+        )
+
+        # Create three issues, one for each project (ids 1:1, 2:2, 3:3)
+        cur.execute(
+            """
+            INSERT INTO issues (id, project_id, title, description)
+            VALUES
+                (1, 1, 'Test Issue 1 for Project 1', 'This is test issue 1 for project 1'),
+                (2, 2, 'Test Issue 2 for Project 2', 'This is test issue 2 for project 2'),
+                (3, 3, 'Test Issue 3 for Project 3', 'This is test issue 3 for project 3')
+            ON CONFLICT (id) DO NOTHING
+        """
+        )
+
+        # Commit the changes and close the connection
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    # If database has connection or other error
+    except psycopg2.Error as e:
+        print(e)
+        sys.exit(-1)
 
 
 if __name__ == "__main__":
