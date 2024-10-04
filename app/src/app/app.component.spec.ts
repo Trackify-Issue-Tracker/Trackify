0c4a97578ed2417425b331a120fef022451e9ad3
svparekh <setulvp@gmail.com>
2024-10-04 13:34:22 -0500

    test

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -1,10 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ApiService } from './api.service';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideRouter(routes), provideHttpClient(withFetch()), ApiService,]
+
     }).compileComponents();
   });
 
