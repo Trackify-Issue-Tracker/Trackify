07db1c5824d7309dce0ac862a66a1a23cf4da093
svparekh <setulvp@gmail.com>
2024-10-06 18:31:20 -0500

    IS_CONTAINERIZED set default true

--- app/src/app/is_containerized.ts
+++ app/src/app/is_containerized.ts
@@ -1,4 +1,4 @@
 // This is used purely to be set by Github Actions when deploying the static version of this site for a live demo.
 // The static version uses cookies to hold data rather than the full backend developed on containers.
 // DO NOT CHANGE
-export const IS_CONTAINERIZED = false
+export const IS_CONTAINERIZED = true
